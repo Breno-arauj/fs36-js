0bd0d7e565ce36afd482486e1bf2c7204361190d
Breno Aragão Araujo Lima <breno6275@DIGITALCOLLEGE.LOCAL>
2024-05-16 21:54:16 -0300

    Variaveis e Dados

---
+++ js/inicio/1-variavel.js
@@ -0,0 +1,29 @@
+// var, const, let
+
+// criando a variavel e atribuindo o valor
+let nome = 'João'
+
+// Não pode ser palavra reservada 
+// Não pode iniciar com número
+// Não pode conter espaço
+// Não pode conter caracteres especiais
+// Não pode iniciar com operadores aritiméticos +,-,/,*
+// Não pode iniciar com caracteres especiais $,%,^,&,*,(,),{,},[,],#,@,!,?,.,:;
+// Não pode iniciar com operadores de comparação  <,>,<=,>=,==,!=,===,!==
+// Não pode iniciar com operadores lógicos &&,||,!,?
+let = 'olá'
+
+// CamelCase: inicia a 1 palavra em minúsculo e o restante das palavras iniciam em maiúsculo
+let sobreNome2 = 'Ferreira'
+
+//let #idade = 12; //erro
+let 1idade = 12; //erro
+let idade1 = 12; //ok
+
+// Alterando o valor
+let nome = 'olá'
+nome = 'Alterei o valor $$@456_'
+console.log(nome);
+// Criando uma constante
+const alturaEverst = 8848;
+alturaEverst = 10500
---
+++ js/inicio/2-tipos_dados.js
@@ -0,0 +1,13 @@
+let n1 = 2,
+n3 = 10;
+n2 = 20
+let n2;
+console.log(n1, n2, n3);
+
+
+//soma
+let numero1 = '100';
+let numero2 = 200;
+// string com o sinal de '+' o js entende como concatenação, ou seja,
+let resultado = numero1 + numero2
+console.log(resultado);
