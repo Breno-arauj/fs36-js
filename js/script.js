39fbe26d113c3378288c90de5e6a8440fbbe52a1
Breno Aragão Araujo Lima <breno6275@DIGITALCOLLEGE.LOCAL>
2024-05-23 20:56:27 -0300

    Subindo exemplos de funções

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Script via body')
+// console.log('Script via body', document.getElementById('n1'))
+
+function soma() {
+    let numero1 = document.getElementById('n1').value;
+    let numero2 = document.getElementById('n2').value;
+
+    let resultado = parseInt(numero1) + parseInt(numero2)
+
+    // console.log(numero1, numero2);
+    console.log(resultado);
+    
+}
--- js/script2.js
+++ js/script2.js
@@ -1,1 +1,24 @@
-console.log('Script via haed')
+/*
+Objetivo: Calcular a média de dois números
+1 - pegar os dois números 
+2 - somar os dois números
+3 - dividir a soma pela quantidade
+*/
+
+let n1 = 100;
+let n2 = 900;
+
+function soma() {
+    console.log(n1+n2)
+    return n1+n2
+}
+    
+    soma()
+
+    function media() {
+console.log(soma () / 2)
+    }
+
+media()
+
+
