eefa240923dc803691998a968b0304e38ae5fe0d
Breno Aragão Araujo Lima <breno6275@DIGITALCOLLEGE.LOCAL>
2024-05-23 21:51:30 -0300

    Subindo atualizações

---
+++ js/inicio/4-arrowFunction.js
@@ -0,0 +1,11 @@
+let nome = 'Breno Aragao'
+
+// function converterMaiscula() {
+//      return nome.toUpperCase();
+//  }
+
+function converterMaiscula(texto) {
+    return texto.toUpperCase();
+}
+
+// console.log(converterMaiscula())
--- js/script2.js
+++ js/script2.js
@@ -9,16 +9,19 @@ let n1 = 100;
 let n2 = 900;
 
 function soma() {
-    console.log(n1+n2)
     return n1+n2
 }
-    
-    soma()
 
-    function media() {
-console.log(soma () / 2)
+function media() {
+     console.log(soma()/2)
     }
 
-media()
+// media()
+
+function media(n1, n2) {
+    let resultado = soma() /2
+    return resultado;
+}
+
 
 
